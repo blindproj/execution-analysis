db51a1e0837b81ed2e75687316f37a903a195b5b
blindproj <carlos_zimmerle@yahoo.com.br>
2019-05-22 01:26:58 -0300

    files update

--- descriptive_statistics.r
+++ descriptive_statistics.r
@@ -1,5 +1,4 @@
 
-treatments <- list("rxjs", "mostjs")
 exp_units <- list("absence", "all", "always", "any", "avgDistance", "count", "decreasing", "increasing",
 "maxDistance", "minDistance", "mixed", "nHighestValues", "nLowestValues", "nonDecreasing", "nonIncreasing",
 "relativeAvgDistance", "relativeMaxDistance", "relativeMinDistance", "sometimes", "stable", "valueAvg", "valueMax",
@@ -7,23 +6,28 @@ exp_units <- list("absence", "all", "always", "any", "avgDistance", "count", "de
 
 frame <- data.frame()
 
-for(exp_unit in exp_units) {
-   for(treatment in treatments){
-		path <- paste("./analysis/", treatment, "/", exp_unit , ".csv", sep = "")
-		
-		myData <- read.csv(file=path, header=FALSE, sep=",")
+for(exp_unit in exp_units){
+	path <- paste("./analysis/By Combination/", exp_unit , ".csv", sep = "")
+	
+	data <- read.csv(file=path, header=TRUE, sep=",")
+	
+	cnt <- 1
+	
+	while (cnt < 5) {
+	
 		frame <- rbind(frame, data.frame(
 			experimental_unit = exp_unit,
-			treatment = treatment,
-			mean = formatC(rowMeans(myData), digits = 5, format= "f"),
-			standard_deviation = formatC(apply(myData, 1, sd), digits = 5, format= "f"),
-			median = apply(myData, 1, median),
-			min = apply(myData, 1, min),
-			max = apply(myData, 1, max)
-		)
+			combination = cnt,
+			min = aggregate( consumption ~ combination, data, min )$consumption[cnt],
+			max = aggregate( consumption ~ combination, data, max )$consumption[cnt],
+			mean = formatC(aggregate( consumption ~ combination, data, mean )$consumption[cnt], digits = 3, format= "f"),
+			standard_deviation = formatC(aggregate( consumption ~ combination, data, sd )$consumption[cnt], digits = 3, format= "f")
+			)
 		)
 		
-   }
+		cnt = cnt + 1
+	
+	}
 }
 
 write.csv(frame,"descriptive_statistics.csv", row.names = FALSE)
--- hypothesis_testing.r
+++ hypothesis_testing.r
@@ -6,32 +6,30 @@ exp_units <- list("absence", "all", "always", "any", "avgDistance", "count", "de
 
 frame <- data.frame()
 
-for(exp_unit in exp_units) {
-   
-		path1 <- paste("./analysis/rxjs/", exp_unit , ".csv", sep = "")
-		path2 <- paste("./analysis/mostjs/", exp_unit , ".csv", sep = "")
-		
-		myData1 <- read.csv(file=path1, header=FALSE, sep=",")
-		myData2 <- read.csv(file=path2, header=FALSE, sep=",")
-		
-		
-		nTest <- wilcox.test(as.vector(t(myData1)), as.vector(t(myData2)), alternative = "greater",
-            mu = 0, paired = FALSE, exact = NULL, correct = FALSE,
-            conf.int = FALSE, conf.level = 0.95)
-		
-		H0 <- "true"
-		if(nTest$p.value < 0.05){
-			H0 <- "false"
-		}
-		
-		frame <- rbind(frame, data.frame(
+for(exp_unit in exp_units){
+	path <- paste("./analysis/By Treatment/", exp_unit , ".csv", sep = "")
+	
+	data <- read.csv(file=path, header=TRUE, sep=",")
+	
+	test <- wilcox.test(consumption ~ treatment, data, paired=FALSE)
+	
+	ranks = aggregate(rank(consumption)~treatment, data, sum )
+	
+	h0 <- FALSE
+	if(test$p.value > 0.05){
+		h0 <- TRUE
+	}
+	
+	frame <- rbind(frame, data.frame(
 			experimental_unit = exp_unit,
-			statistic = nTest$statistic,
-			p.value = nTest$p.value,
-			null_hypothesis = H0
+			statistics = test$statistic,
+			p_value = formatC(test$p.value, digits = 3, format= "f"),
+			h0 = h0,
+			most_rank = ranks$rank[1],
+			rxjs_rank = ranks$rank[2]
+			)
 		)
-		)
-		
 }
 
 write.csv(frame,"hypothesis_testing.csv", row.names = FALSE)
+	
